dd79f43196f1bae612ccc73f461a2b1f751211a4
Shubham Singh <shubhamsinghjhitti143@gmail.com>
2024-02-12 02:37:41 +0530

    image inserted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./component/layout/Header/Header.js";
+import { BrowserRouter as Router,} from "react-router-dom";
+import WebFont from "webfontloader";
+import React from "react";
+import Footer from "./component/layout/Footer/Footer.js";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    React.useEffect(() => {
+      WebFont.load({
+        google: {
+          families: ["Roboto", "Droid Sans", "Chilanka"],
+        },
+      });
+  
+    
+    }, []);
+    return (
+      <Router>
+       <Header />
+       <Footer />
+      </Router>
+    );
+  
 }
 
 export default App;
